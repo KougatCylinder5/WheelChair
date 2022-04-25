45bd70296d8a96bdb42dd8beddd68dbb995718a5
KougatCylinder5 <58046422+KougatCylinder5@users.noreply.github.com>
2022-04-25 11:49:37 -0400

    Basic Motor Control

--- Wheelchair.py
+++ Wheelchair.py
@@ -1,3 +1,22 @@
 import numpy
 import cv2
+from Adafruit_MotorHAT import Adafruit_MotorHAT
+import time
 
+MoHat = Adafruit_MotorHAT(addr=0x60)
+
+M3 = 3
+M4 = 4
+
+MotorLeft = MoHat.getMotor(M4)
+MotorRight = MoHat.getMotor(M3)
+
+motor_speed = 100
+
+MotorLeft.setSpeed(motor_speed)
+MotorRight.setSpeed(motor_speed)
+
+print(MoHat.FORWARD)
+MotorLeft.run(MoHat.FORWARD)
+time.sleep(2.25)
+MotorLeft.run(MoHat.RELEASE)
