037880a74a79ae62b4f1ff3605cb778c49f77bd7
KougatCylinder5 <58046422+KougatCylinder5@users.noreply.github.com>
2022-05-04 11:54:00 -0400

    5/4/22
    Addons
    -Added sections to the UI window
    -Added arrows to the UI window to signifiy direction
    
    Changes
    - inverted 0x so that going up makes the mouse move up instead of down ie  (-0x)

--- WheelchairCamera.py
+++ WheelchairCamera.py
@@ -9,14 +9,22 @@ if(not vid.isOpened()):
     print("Camera Failed to Load... Exiting")
     time.sleep(2) # 
     exit() # exit program
-height, width = GUI.size()
-print(height, width)
-UI = numpy.zeros((width,height,3))
-UI[round(width/3):round((width/3)*2),round(height/3):round((height/3)*2)] = [0,0,255]
-UI[0:round(width/3),0:round(height/3)] = [255,0,0]
-UI[0:round(width/3),round(height/3) * 2:height] = [255,0,0]
-UI[round(width/3) * 2:width,0:round(height/3)] = [0,255,0]
-UI[round(width/3) * 2:width,round(height/3) * 2:height] = [0,255,0]
+width, height = GUI.size()
+UI = numpy.zeros((height,width,3), numpy.uint8)
+#UI[round(width/3):round((width/3)*2),round(height/3):round((height/3)*2)] = [0,0,255]
+leftArrow = numpy.array([[width/9,height/1.5],[width/ 20, height/2],[width/9,height/3],[width/ 20, height/2],[width/ 4, height/2]],numpy.int32) 
+# co-ordinates for the arrow
+leftArrow = leftArrow.reshape((-1, 1, 2)) # reshapes the arrow co-ordinates so that ploylines can accept it
+
+rightArrow = numpy.array([[width/1.12,height/1.5],[width/ 1.05, height/2],[width/1.12,height/3],[width/ 1.05, height/2],[width/ 1.35, height/2]],numpy.int32) 
+rightArrow = rightArrow.reshape((-1, 1, 2)) # reshapes the arrow co-ordinates so that ploylines can accept it
+# co-ordinates for the arrow
+
+UI[0:height,0:round(width/3)] = [50,50,50]
+UI[0:height,round(width/3) * 2:width] = [50,50,50]
+cv2.polylines(UI,[leftArrow],False,(0,0,255),10)
+cv2.polylines(UI,[rightArrow],False,(0,255,0),10)
+
 cv2.namedWindow('Video') # create window for video
 cv2.namedWindow('Wheelchair Controller',cv2.WINDOW_FULLSCREEN)
 while True:
@@ -35,7 +43,7 @@ while True:
         x0 = (x - len(frame[0])/2) * 0.5 # split the found point in half, so that it becomes negative on the left and positive on the right
         y0 = (y - len(frame)/2) * 0.5 # same as above
         if(abs(x0) > 15 or abs(y0) > 15):
-            GUI.moveRel(x0,y0) # move the mouse by the (x0,y0)
+            GUI.moveRel(-x0,y0) # move the mouse by the (x0,y0)
     except:
         pass # pass the exception so that it doesn't error
     cv2.imshow('Video',frame) # show the frame
